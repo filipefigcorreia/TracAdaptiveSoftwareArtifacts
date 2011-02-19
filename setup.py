51a4a60c5290be7bf59f11e7e412b8c6d8db0a57
Filipe Correia <fcorreia@gmail.com>
2011-02-19 21:41:20 +0000

    Added some author info to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ VERSION = '0.1'
 
 setup(name=PACKAGE,
     version=VERSION,
+    author='Filipe Correia',
+    author_email='filipe dot correia at fe dot up dot pt',
+    long_description="""
+      This Trac plugin allows to create entities following an arbitrary structure, improving developers' expressiveness.
+      """,
     packages=['AdaptiveArtifacts'],
     entry_points={'trac.plugins': '%s = AdaptiveArtifacts.adaptive_artifacts' % PACKAGE},
     package_data={'AdaptiveArtifacts': ['htdocs/css/*.css',
