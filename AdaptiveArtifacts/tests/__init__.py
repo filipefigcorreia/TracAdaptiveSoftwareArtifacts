16ce44c95411ac36fec911a7f2da85edf79013cf
Filipe Correia <fcorreia@gmail.com>
2012-02-03 12:11:29 +0000

    Adding persistence and request-handling tests to the tests __init__ file

--- AdaptiveArtifacts/tests/__init__.py
+++ AdaptiveArtifacts/tests/__init__.py
@@ -1,6 +1,8 @@
 import unittest
 
 from AdaptiveArtifacts.tests.model import *
+from AdaptiveArtifacts.tests.persistence import *
+from AdaptiveArtifacts.tests.request_handling import *
 
 if __name__ == '__main__':
     unittest.main()
