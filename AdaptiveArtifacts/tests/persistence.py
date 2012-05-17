e66dd0001c2e1d87cfb3ad9515d1b8ebc7c334ff
Filipe Correia <fcorreia@gmail.com>
2012-05-17 17:03:21 +0100

    Cleanup

--- AdaptiveArtifacts/tests/persistence.py
+++ AdaptiveArtifacts/tests/persistence.py
@@ -3,12 +3,8 @@
 # This software is licensed as described in the file license.txt, which
 # you should have received as part of this distribution.
 
-import shutil
-import tempfile
 import unittest
 from trac.test import EnvironmentStub, Mock
-from trac.util import get_reporter_id
-from trac.web.api import Request, RequestDone
 from AdaptiveArtifacts.persistence.db import Setup
 from AdaptiveArtifacts.persistence.data import DBPool
 from AdaptiveArtifacts.model.core import Instance, Entity, Attribute
@@ -40,22 +36,6 @@ class BasicEntityBehaviour(unittest.TestCase):
         lightningMcQueen = pool.get_item(self.lightningMcQueen.get_id())
         self.assertEqual(lightningMcQueen.get_id(), self.lightningMcQueen.get_id())
         self.assertEqual(len(lightningMcQueen.get_value("Wheels")), 4)
-
-"""
-    def test_properties(self):
-        dbp = DBPool(self.env)
-        pinstances = dbp.load_instances_of(ppool.pool.get_instance_by_iname('__property').get_identifier())
-        self.assertEqual(len(pinstances), 1, "Wrong number of properties. Expected %r. Got %r.")
-        self.assertEqual(self.wheels.get_identifier(), pinstances[0].instance.get_identifier(), "Wrong property. Expected %r. Got %r.")
-
-    def test_property_values(self):
-        ppool = PersistablePool.load(self.env)
-        pi_car = ppool.get_instance(self.env, self.car.get_identifier())
-        pi_a_car = ppool.get_instance(self.env, self.lightningMcQueen.get_identifier())
-        values = pi_a_car.instance.get_value(self.wheels.get_identifier())
-        self.assertTrue(isinstance(values, list), "Expected values with multiplicity > 1 ('%s')" % (values,))
-        self.assertEqual(4, len(values), "Wrong number of wheels. Expected %r. Got %r." % (4, len(values)))
-"""
 
 class Scenarios(object):
     @staticmethod
