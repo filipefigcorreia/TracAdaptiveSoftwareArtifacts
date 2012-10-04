a00a6076056f00e5799cf2a78520c05c972ce306
Filipe Correia <fcorreia@gmail.com>
2012-10-04 13:42:06 +0100

    Heavy refactoring of dialogs.js

--- AdaptiveArtifacts/htdocs/js/dialogs.js
+++ AdaptiveArtifacts/htdocs/js/dialogs.js
@@ -1,56 +1,64 @@
 $(document).ready(function(){
-    $('a#dialog').click(showNewArtifactDialog);
+    $('.opendialog.newartifact').click( function() { showCreateArtifactDialog() });
 });
 
-showNewArtifactDialog = function(){
-    var dlg = $('.dialog');
-    var tag = createDialogFromUrl(
+showCreateArtifactDialog = function(){
+    createASADialogFromUrl(
             "/my-trac-environment/adaptiveartifacts/artifact?action=new&format=dialog",
             {
                 title: 'Artifact',
-                success: function(data, textStatus, jqXHR){
-                    attachFormEventHandlers($(".dialog"));
-                },
-                buttons: {"Create": function() {
-                    var form=$('.dialog').find('form');
-                    $.ajax({
-                        type: form.attr('method'),
-                        url: form.attr('action'),
-                        data: form.serialize(),
-                        context: $(this), //so that we can call $(this) on the success and error callbacks
-                        success: function(data){
-                            $(this).dialog("close");
-                            $(this).dialog("destroy");
-                            $(this).remove();
-                        },
-                        error: function(data){
-                            $(".dialog").html("An error occurred, sorry.");
-                            $(this).dialog("option", "buttons", [
-                                {
-                                    text: "Close",
-                                    click: function() {
-                                        $(this).dialog("close");
-                                        $(this).dialog("destroy");
-                                        $(this).remove();
-                                    }
-                                }
-                            ] );
-                        }
-                    });
-                }}
+                success: function(data, textStatus, jqXHR) { attachFormEventHandlers($(this)) },
+                buttons: { "Create": function() { submitDialogForm($(this)) } }
             }
-        );
+        ).dialog('open');
+}
+
+/*
+Submits a jquery-ui dialog. The dialog is closed if
+submission goes flawlessly, and shows an error message
+otherwise.
+ */
+submitDialogForm = function(dialogdiv) {
+    var form=dialogdiv.find('form');
+    $.ajax({
+        type: form.attr('method'),
+        url: form.attr('action'),
+        data: form.serialize(),
+        context: dialogdiv, //so that we can call $(this) on the success and error callbacks
+        success: function(data){
+            $(this).dialog("close");
+            $(this).dialog("destroy");
+            $(this).remove();
+        },
+        error: function(data){
+            $(this).html("An error occurred, sorry.");
+            $(this).dialog("option", "buttons", [
+                {
+                    text: "Close",
+                    click: function() {
+                        $(this).dialog("close");
+                        $(this).dialog("destroy");
+                        $(this).remove();
+                    }
+                }
+            ] );
+        }
+    });
+}
+
+function createASADialogFromUrl(url, options){
+    var dialogdiv = createDialogFromUrl(url, options, 'asa-dialog');
     $('<input>').attr({
         type: 'hidden',
         name: 'format',
         value: 'dialog'
-    }).appendTo($('.dialog').find('form'));
-    tag.dialog('open');
+    }).appendTo(dialogdiv.find('form'));
+    return dialogdiv;
 }
 
-function createDialogFromUrl(url, options){
+function createDialogFromUrl(url, options, dialogdivclass){
   options = options || {};
-  var tag = $('<div class="dialog"></div>');
+  var dialogdiv = $('<div class="{0}"></div>'.format(dialogdivclass));
   $.ajax({
     url: url,
     type: (options.type || 'GET'),
@@ -59,13 +67,12 @@ function createDialogFromUrl(url, options){
     complete: options.complete,
     success: function(data, textStatus, jqXHR) {
       if(typeof data == "object" && data.html) { //response is assumed to be JSON
-        tag.html(data.html).dialog({modal: options.modal, title: data.title, buttons: options.buttons});
+        dialogdiv.html(data.html).dialog({modal: options.modal, title: data.title, buttons: options.buttons});
       } else { //response is assumed to be HTML
-        tag.html(data).dialog({modal: options.modal, title: options.title, buttons: options.buttons});
+        dialogdiv.html(data).dialog({modal: options.modal, title: options.title, buttons: options.buttons});
       }
-      data.tag = tag;
-      $.isFunction(options.success) && (options.success)(data, textStatus, jqXHR);
+      $.isFunction(options.success) && (options.success).call(dialogdiv, data, textStatus, jqXHR);
     }
   });
-  return tag;
+  return dialogdiv;
 }
