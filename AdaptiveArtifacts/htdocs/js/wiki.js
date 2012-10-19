60dbef064941a38d9b5b6ced147fb8ff6cbe2445
Fabio <fabcpinho@gmail.com>
2012-10-19 16:31:37 +0100

    * Fixed bug in which calling toString on a range would return extra whitespaces;  * Unified the window.onload with the document.ready event  * Added exception control to the timer code (queue processing)

--- AdaptiveArtifacts/htdocs/js/wiki.js
+++ AdaptiveArtifacts/htdocs/js/wiki.js
@@ -10,7 +10,7 @@ var words_array = new Array("database", "extra", "simply", "TracEnvironment", "t
 
 function highlightRanges(ranges) {
     for (var i = ranges.length - 1; i >= 0; i--) {
-        if ((words_array.indexOf(ranges[i].toString())) != -1) {
+        if ((words_array.indexOf(ranges[i].text())) != -1) {
             searchResultApplier.applyToRange(ranges[i]);
         } else {
             //TODO: retirar span no caso de estarmos dentro de um
@@ -21,42 +21,10 @@ function highlightRanges(ranges) {
         }
     }
 }
-window.onload = function() {
-    rangy.init();
-
-    searchResultApplier = rangy.createCssClassApplier("divtext_selected_word");
-
-    highlighter = rangy.createHighlighter();
-
-    highlighter.addCssClassApplier(rangy.createCssClassApplier("highlight", {
-        ignoreWhiteSpace: true,
-        tagNames: ["span", "a"]
-    }));
-
-    highlighter.addCssClassApplier(rangy.createCssClassApplier("note", {
-        ignoreWhiteSpace: true,
-        elementTagName: "a",
-        elementProperties: {
-            href: "#",
-            onclick: function() {
-                var highlight = highlighter.getHighlightForElement(this);
-                if (window.confirm("Delete this note (ID " + highlight.id + ")?")) {
-                    highlighter.removeHighlights( [highlight] );
-                }
-                return false;
-            }
-        }
-    }));
-
-    var fullRange = rangy.createRange();
-    fullRange.selectNodeContents(document.getElementById("divtext").childNodes[0]);
-    var splittedRanges = splitRange(fullRange);
-    highlightRanges(splittedRanges);
-};
 
 
 function splitRange(fullRange) {
-    var fullRangeText = fullRange.toString().split('\n').join(' ');
+    var fullRangeText = fullRange.text().split('\n').join(' ');
 
     var start = 0;
     var idx = 0;
@@ -88,15 +56,36 @@ $(document).ready(function(){
     var text = $('#divtext');
 
 
-    /*var counter = 0;
-    while(counter<words_array.length){
-        var reg=new RegExp(words_array[counter], "g");
-        var subst =  "<span class='.divtext_selected_word' style='color:red'>" + words_array[counter] +  "</span>";
-        text.html( text.html().replace(reg, subst)) ;
-        counter++;
-    }*/
+    rangy.init();
 
+    searchResultApplier = rangy.createCssClassApplier("divtext_selected_word");
 
+    highlighter = rangy.createHighlighter();
+
+    highlighter.addCssClassApplier(rangy.createCssClassApplier("highlight", {
+        ignoreWhiteSpace: true,
+        tagNames: ["span", "a"]
+    }));
+
+    highlighter.addCssClassApplier(rangy.createCssClassApplier("note", {
+        ignoreWhiteSpace: true,
+        elementTagName: "a",
+        elementProperties: {
+            href: "#",
+            onclick: function() {
+                var highlight = highlighter.getHighlightForElement(this);
+                if (window.confirm("Delete this note (ID " + highlight.id + ")?")) {
+                    highlighter.removeHighlights( [highlight] );
+                }
+                return false;
+            }
+        }
+    }));
+
+    var fullRange = rangy.createRange();
+    fullRange.selectNodeContents(document.getElementById("divtext").childNodes[0]);
+    var splittedRanges = splitRange(fullRange);
+    highlightRanges(splittedRanges);
 
 
 
@@ -162,58 +151,62 @@ $(document).ready(function(){
             clearTimeout(timeout);
         }
 
-        //var newRange = rangy.createRange();
-        //newRange.selectNodeContents(document.getElementsByClassName("divtext")[0]);
 
         var newRange = rangy.getSelection().getRangeAt(0);
 
         var oldRange = Ranges[Ranges.length-1];
-        if(oldRange && oldRange.endContainer == newRange.startContainer && oldRange.endOffset >= newRange.startOffset-3){
-            Ranges[Ranges.length-1].setEnd(newRange.endContainer, newRange.endOffset);
-            console.log("Atualizei o seguinte: ");
-            console.log( (Ranges[Ranges.length-1]).toString());
+        if(oldRange &&
+              oldRange.endContainer == newRange.startContainer &&
+                oldRange.endOffset >= newRange.startOffset-3){
+            if (newRange.endOffset > Ranges[Ranges.length-1].endOffset){
+                Ranges[Ranges.length-1].setEnd(newRange.endContainer, newRange.endOffset);
+                console.log("Atualizei o seguinte: ");
+                console.log( (Ranges[Ranges.length-1]).text());
+            }
         }else{
             console.log("Comecei o seguinte: ");
-            console.log(newRange.toString());
+            console.log(newRange.text());
             Ranges.push(newRange);
         }
-        //console.log(Ranges[0].cloneRange());
-        //console.log(Ranges[0].toString());
-
         reloadSearch();
-
     });
 
 
     function reloadSearch() {
         //Memory Leak alert!
+
+
         if (!isLoading) {
-
             timeout = setTimeout(function() {
+                try {
+                    isLoading = true;
+                    //Analise da pagina
+                    var selection1 = rangy.saveSelection(window);
 
-                isLoading = true;
-                //Analise da pagina
-                var selection1 = rangy.saveSelection(window);
+                    var i = 0;
+                    var splittedRanges = [];
+                    while (Ranges.length>0) {
 
-
-                var i = 0;
-                var splittedRanges = [];
-                while (Ranges.length>0) {
-
-                    var fullRange = Ranges.shift();
-                    console.log("Consumi o seguinte: ");
-                    console.log(fullRange.toString());
-                    var ranges = splitRange(fullRange);
-                    $.merge(splittedRanges, ranges);
+                        var fullRange = Ranges.shift();
+                        console.log("Consumi o seguinte: ");
+                        console.log(fullRange.text());
+                        var ranges = splitRange(fullRange);
+                        $.merge(splittedRanges, ranges);
+                    }
+                    highlightRanges(splittedRanges);
+                    rangy.restoreSelection(selection1);
+                    //Fim da analise
                 }
-                highlightRanges(splittedRanges);
+                catch(exception){
+                    console.log("Exception while processing queue:");
+                    console.log(exception);
+                }
+                finally {
+                    isLoading = false;
+                }
 
-                //
-                rangy.restoreSelection(selection1);
-                //Fim da analise
 
-                // Simulate a real ajax call
-                setTimeout(function() { isLoading = false; }, 500);
+
             }, delay);
 
         }
@@ -223,14 +216,6 @@ $(document).ready(function(){
     }
 
     /*$('#divtext').bind('click', function() {
-
-
-
-        var searchScopeRange = rangy.createRange();
-        searchScopeRange.selectNodeContents(document.getElementById("divtext"));
-
-        console.log(searchScopeRange);
-
 
     });*/
 
@@ -258,7 +243,7 @@ function changeTagOnSelectedString(prefix, sufix){
     }
 };
 
-
+//Sera mesmo necessario? Substituir pelo rangy
 function insertHtmlAfterSelection(html) {
     var sel, range;
     if (window.getSelection) {
