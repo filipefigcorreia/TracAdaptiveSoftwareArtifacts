08652234f7df61e9b11946b85c430c09d7c285a3
Filipe Correia <fcorreia@gmail.com>
2012-11-16 23:33:00 +0000

    Added some semicolons to have the IDE off my back.

--- AdaptiveArtifacts/htdocs/js/forms.js
+++ AdaptiveArtifacts/htdocs/js/forms.js
@@ -18,19 +18,19 @@ function attachFormEventHandlers(context){
 }
 
 function addAttribute(){
-    var newid = uuid.v4()
-    var copy = $("tr.attribute:last").clone(true)
-    copy.removeClass('prototype')
-    var name_input = copy.find("input[name^='attr-name-']")
-    name_input.attr("name", "attr-name-" + newid)
-    name_input.attr("value", "")
-    var type_input = copy.find("select[name^='attr-type-']")
-    type_input.attr("name", "attr-type-" + newid)
-    type_input.attr("value", "")
-    var type_multiplicity = copy.find("select[name^='attr-multiplicity-']")
-    type_multiplicity.attr("name", "attr-multiplicity-" + newid)
-    type_multiplicity.attr("value", "")
-    $("table.attributes").append(copy)
+    var newid = uuid.v4();
+    var copy = $("tr.attribute:last").clone(true);
+    copy.removeClass('prototype');
+    var name_input = copy.find("input[name^='attr-name-']");
+    name_input.attr("name", "attr-name-" + newid);
+    name_input.attr("value", "");
+    var type_input = copy.find("select[name^='attr-type-']");
+    type_input.attr("name", "attr-type-" + newid);
+    type_input.attr("value", "");
+    var type_multiplicity = copy.find("select[name^='attr-multiplicity-']");
+    type_multiplicity.attr("name", "attr-multiplicity-" + newid);
+    type_multiplicity.attr("value", "");
+    $("table.attributes").append(copy);
 }
 
 function delAttribute(){
