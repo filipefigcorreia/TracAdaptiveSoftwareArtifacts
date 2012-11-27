d92722a2f5f99f5b25e94cccb4669d5d812cf637
Filipe Correia <fcorreia@gmail.com>
2012-11-27 10:56:17 +0000

    Added some semicolons, to make the IDE stop nagging me

--- AdaptiveArtifacts/htdocs/js/forms.js
+++ AdaptiveArtifacts/htdocs/js/forms.js
@@ -46,26 +46,26 @@ function addAttribute(){
 }
 
 function delAttribute(){
-    $(this).parent().parent().remove()
+    $(this).parent().parent().remove();
 }
 
 function addValue(context, name, val){
-    var newid = uuid.v4()
-    var copy = context.find("tr.attribute.prototype").clone(true)
-    copy.removeClass('prototype')
-    var name_input = copy.find("input[name^='attr-name-']")
-    name_input.attr("name", "attr-name-" + newid)
-    name_input.attr("value", name)
-    var value_input = copy.find("input[name^='attr-value-']")
-    value_input.attr("name", "attr-value-" + newid)
-    value_input.attr("value", val)
-    var default_input = copy.find("input[name^='default']")
-    default_input.attr("value", newid)
+    var newid = uuid.v4();
+    var copy = context.find("tr.attribute.prototype").clone(true);
+    copy.removeClass('prototype');
+    var name_input = copy.find("input[name^='attr-name-']");
+    name_input.attr("name", "attr-name-" + newid);
+    name_input.attr("value", name);
+    var value_input = copy.find("input[name^='attr-value-']");
+    value_input.attr("name", "attr-value-" + newid);
+    value_input.attr("value", val);
+    var default_input = copy.find("input[name^='default']");
+    default_input.attr("value", newid);
     if ($('form#artifact-form input[name=default]:checked').length==0)
-        default_input.attr('checked', true)
-    context.find("table.attributes").append(copy)
+        default_input.attr('checked', true);
+    context.find("table.attributes").append(copy);
 }
 
 function delValue(){
-    $(this).parent().parent().remove()
+    $(this).parent().parent().remove();
 }
