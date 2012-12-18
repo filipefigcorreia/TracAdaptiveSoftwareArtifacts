e9a578a2f7f25b653e06dc58f01582ba701cfe35
Filipe Correia <fcorreia@gmail.com>
2012-12-18 12:43:04 +0000

    Updated version number.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 PACKAGE = 'AdaptiveArtifacts'
-VERSION = '0.1'
+VERSION = '0.2'
 
 setup(name=PACKAGE,
     version=VERSION,
     author='Filipe Correia',
     author_email='filipe dot correia at fe dot up dot pt',
     long_description="""
-      This Trac plugin allows to create information following an arbitrary structure, improving developers' expressiveness.
+      This Trac plugin allows to create information following an arbitrary structure, that can emerge during a project's lifetime, rather than having to be established at installation/configuration time.
       """,
     packages=find_packages(exclude=['*.tests']),
     entry_points={
